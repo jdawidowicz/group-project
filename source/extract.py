1017a144c2252454f477918e02e90aa508d56760
Joseph Beckett <josephbeckett2708@gmail.com>
2022-12-14 11:32:23 +0000

    Created Problems on Purpose

--- source/extract.py
+++ source/extract.py
@@ -5,24 +5,3 @@
 # File Last Edited 05/12/22
 ######################################################
 
-## Imports
-import pandas as pd
-import glob
-import os
-
-
-## Functions
-# Reads CSV file
-def read_files():
-    #create list to hold dataframes
-    list_of_df = []
-
-    
-    # Reads CSV file and returns it as a dataframe
-    #The 'glob' function will look through the 'data' folder for any file enifing in '.csv'
-    for csv in glob.glob("data\*.csv"):
-        df = pd.read_csv(csv, names=['date', 'branch', 'name', 'product', 'total_price', 'payment_type', 'card_details'])
-        list_of_df.append(df)
-
-    
-    return list_of_df
