c8ff7ecadeb286cd9d17c44a0040d2dd3eeb17e3
IllingworthO <illingwortho@gmail.com>
2023-01-12 15:27:20 +0000

    Changes to lambda code

--- lambda_code/lambdacode.py
+++ lambda_code/lambdacode.py
@@ -16,16 +16,17 @@ def lambda_handler(event, context):
   csv_object = s3_client.get_object(Bucket=bucket,Key=csv_file_name)
   df = read_files(csv_object)
   df = format_df(df)
-  print(df.head(10))
+ #print(df.head(10))
 
   #create and load orders table
-  #load_orders_table(df)
+  load_orders_table(df)
 
   #create and load products table
   #load_products_table(df)
 
   #create and load basket table
   #load_baskets(df)
+  
   return {
     'statusCode': 200,
     'body': json.dumps('Hello from Lambda!')
--- lambda_code/sql_utils.py
+++ lambda_code/sql_utils.py
@@ -2,63 +2,76 @@ import psycopg2
 import os
 #from dotenv import load_dotenv
 from sqlalchemy import create_engine
+import boto3
+import json
+#import redshift_connector
 
+ssm = boto3.client('ssm')
+parameter = ssm.get_parameter(Name='team1', WithDecryption=True)
+secret_json = json.loads(parameter['Parameter']['Value'])
+#{"username":"team1","password":"Ghio4*(2dxc","engine":"redshift","host":"redshiftcluster-uskxazijhsjf.cfpmkbr0g5vs.eu-west-1.redshift.amazonaws.com","port":5439,"dbClusterIdentifier":"redshiftcluster-uskxazijhsjf"}
 
-HOST='localhost'
-USER='postgres'
-PASSWORD='password'
-DB_NAME='database'
+HOST=secret_json['host']
+USER=secret_json['username']
+PASSWORD=secret_json['password']
+DB_NAME=secret_json['database']
+PORT=secret_json['port']
 
-# load_dotenv()
-# HOST = os.environ.get("POSTGRES_host")
-# USER = os.environ.get("POSTGRES_user")
-# PASSWORD = os.environ.get("POSTGRES_pass")
-# DB_NAME = os.environ.get("POSTGRES_db")
-
-DB_DATA = 'postgresql+psycopg2://' + USER + ':' + PASSWORD + '@' + 'localhost' + ':5432/' \
+DB_DATA = 'postgresql+psycopg2://' + USER + ':' + PASSWORD + '@' + HOST + ':5439/' \
        + DB_NAME
 # + '?charset=utf8mb4'
 #f"postgresql+psycopg2://{USER}:{PASSWORD}@localhost:5432\{DB_NAME}'?charset=utf8mb4"
 
 
-def setup_db_connection(host=HOST, user=USER, password=PASSWORD, db_name=DB_NAME):
+def setup_db_connection(host=HOST, user=USER, password=PASSWORD, port=PORT, db_name=DB_NAME):
 
-    connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
+    connection = psycopg2.connect(host=host, user=user, password=password, port=port, database=db_name)
     cursor = connection.cursor()
+    
+    # connection = redshift_connector.connect(
+    #     host=host,
+    #     database='dev',
+    #     port='5439',
+    #     user='awsuser',
+    #     password='my_password'
+    # )
+  
+    #cursor = connection.cursor()
+    
     return connection, cursor
 
 def create_db_tables(connection, cursor):
     
-    create_basket_data_table = \
-    """
-        CREATE TABLE IF NOT EXISTS basket(
-            order_id serial NOT NULL,
-            product varchar(50),
-            product_id int NULL
-        );
-    """;
-    create_product_data_table = \
-    """
-        CREATE TABLE IF NOT EXISTS products(
-            product_id serial NOT NULL,
-            product varchar(50),
-            price float
-        );
-    """;
+    # create_basket_data_table = \
+    # """
+    #     CREATE TABLE IF NOT EXISTS basket(
+    #         order_id int NOT NULL AUTO_INCREMENT,
+    #         product varchar(50),
+    #         product_id int NULL
+    #     );
+    # """;
+    # create_product_data_table = \
+    # """
+    #     CREATE TABLE IF NOT EXISTS products(
+    #         product_id int NOT NULL AUTO_INCREMENT,
+    #         product varchar(50),
+    #         price float
+    #     );
+    # """;
     create_order_data_table = \
     """
-        CREATE TABLE IF NOT EXISTS orders(
-            order_id serial NOT NULL ,
+        CREATE TABLE IF NOT EXISTS team1orders(
+            
             date varchar(20),
             payment_type varchar(7),
-            total_price float,
+            total_price decimal,
             branch varchar(20)
         );
     """;
-
+    #order_id int smallseria,
     
-    cursor.execute(create_basket_data_table)
-    cursor.execute(create_product_data_table)
+    #cursor.execute(create_basket_data_table)
+    #cursor.execute(create_product_data_table)
     cursor.execute(create_order_data_table)
     connection.commit()
     cursor.close()
